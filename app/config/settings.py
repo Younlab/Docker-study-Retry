461945c3e2483fbeebc12830a543bb7798507328
Sehyeon Youn <d.sehyeon@gmail.com>
2018-07-11 13:32:02 +0900

    root url setting

--- app/config/settings.py
+++ app/config/settings.py
@@ -14,7 +14,13 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+ROOT_DIR = os.path.dirname(BASE_DIR)
+STATIC_DIR = os.path.join(ROOT_DIR, '.static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+MEDIA_ROOT = os.path.join(ROOT_DIR, '.media')
+MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
